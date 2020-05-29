f1e7737b2f6a337865f7707bc9bb85e2461edf9c
lushuaihua <zhuaiman@hotmail.com>
2020-05-29 13:33:12 +0800

    example完善

---
+++ example/example_Des.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	demo "github.com/lulouis/marisfrolg_utils"
+)
+
+func main() {
+	substring := demo.Substr("marisfrolg_utils", 0, 10)
+	fmt.Println(substring)
+
+}
