efcafb585c5995bdd65f5c3805c1a55ea83fa750
peaklee_forever <lf1988103@126.com>
2020-12-21 09:42:28 +0800

    测试更新

--- example/SendMessage_test.go
+++ example/SendMessage_test.go
@@ -7,6 +7,6 @@ import (
 )
 
 func TestSendMessage(t *testing.T) {
-	r := marisfrolg_utils.SendShortMessage("regionId","accessKeyId","accessKeySecret","12345678910","测试","qwerty","")
+	r := marisfrolg_utils.SendShortMessage("hangzhou","LTAI4G6joAA8gwV3pd1FUoG4","accessKeySecret","13534023573","测试","qwerty","")
 	fmt.Println(r)
 }
