6ee569c306f4915488bf94fc3700942691e3c1df
panpeizhao <987265644@qq.com>
2023-02-20 14:46:33 +0800

    消息队列, 生产推送方法扩展

--- SendQueueMessage.go
+++ SendQueueMessage.go
@@ -14,6 +14,15 @@ import (
 
 var conn *amqp.Connection
 
+type SendQueueMessageInput struct {
+	CurrentMode          string
+	RabbitmqConn         string
+	BusinessName         string
+	MessageBody          interface{}
+	QueueIssueCollection *mgo.Collection
+	MongoSession         *mgo.Session
+}
+
 func SendQueueMessage(currentMode string, rabbitmq_conn string, businessName string, messageBody interface{}, mongoSession *mgo.Session) (err error) {
 	defer func() {
 		if err := recover(); err != nil {
@@ -125,6 +134,126 @@ func SendQueueMessage(currentMode string, rabbitmq_conn string, businessName str
 	if err != nil {
 		//记录mongo日志
 		if currentMode == "PRD" {
+			c.Insert(issue)
+		}
+		err = errors.New(queueName + "发送队列消息失败")
+		return
+	}
+
+	return
+}
+
+func SendQueueMessageV2(input *SendQueueMessageInput) (err error) {
+	defer func() {
+		if err := recover(); err != nil {
+			logBody := bson.M{}
+			logBody["currentMode"] = input.CurrentMode
+			logBody["rabbitmq_conn"] = input.RabbitmqConn
+			logBody["businessName"] = input.BusinessName
+			logBody["messageBody"] = input.MessageBody
+			body, _ := json.Marshal(logBody)
+			AddOperationLog("marisfrolg_utils", "SendQueueMessageV2", fmt.Sprintf("错误详情:%s\n传入参数:%s \n", err, string(body)), "Log")
+		}
+	}()
+	c := input.QueueIssueCollection
+	issue := bson.M{}
+	issue["_id"] = bson.NewObjectId()
+	// 声明一个队列
+	queueName := ""
+	switch input.CurrentMode {
+	case "DEV":
+		queueName = input.BusinessName + "_dev"
+	case "TEST":
+		queueName = input.BusinessName + "_dev"
+	case "PRD":
+		queueName = input.BusinessName + "_prd"
+		issue["queueName"] = queueName
+		issue["queueDualConn"] = input.RabbitmqConn
+		issue["queueMessage"] = input.MessageBody
+		issue["currentMode"] = input.CurrentMode
+		issue["createTime"] = time.Now()
+		issue["finishedStatus"] = 0 //0未处理,1已完结,2取消处理
+	}
+	// 连接RabbitMQ服务器
+	if conn == nil || conn.IsClosed() {
+		conn, err = amqp.Dial(input.RabbitmqConn)
+		if err != nil {
+			//记录mongo日志
+			if input.CurrentMode == "PRD" {
+				c.Insert(issue)
+			}
+			return
+		}
+	}
+
+	// 创建一个channel
+	ch, err := conn.Channel()
+	if err != nil {
+		//记录mongo日志
+		if input.CurrentMode == "PRD" {
+			c.Insert(issue)
+		}
+		return
+	}
+	defer ch.Close()
+	//交换机检查x-dead-letter-exchange-all-business
+	ch.ExchangeDeclare("x-dead-letter-exchange-all-business", "direct", true, false, false, false, nil)
+	// 创建本业务死信
+	ch.QueueDeclare(
+		input.BusinessName+"_dead", // 队列名称
+		true,                       // 是否持久化
+		false,                      // 是否自动删除
+		false,                      // 是否独占
+		false, nil,
+	)
+	ch.QueueBind(input.BusinessName+"_dead", input.BusinessName+"_dead", "x-dead-letter-exchange-all-business", false, nil)
+
+	args := make(map[string]interface{}, 0)
+	args["x-dead-letter-exchange"] = "x-dead-letter-exchange-all-business"
+	args["x-dead-letter-routing-key"] = input.BusinessName + "_dead"
+	q, err := ch.QueueDeclare(
+		queueName, // 队列名称
+		true,      // 是否持久化
+		false,     // 是否自动删除
+		false,     // 是否独占
+		false, args,
+	)
+	if err != nil {
+		//记录mongo日志
+		if input.CurrentMode == "PRD" {
+			c.Insert(issue)
+		}
+		err = errors.New("连接" + queueName + "队列时失败")
+		return
+	}
+	// 发送消息到队列中
+	// body := fmt.Sprintf(`{"name":"刘宇辉","id":1}`)
+	body, err := json.Marshal(input.MessageBody)
+	if err != nil {
+		//记录mongo日志
+		if input.CurrentMode == "PRD" {
+			c.Insert(issue)
+		}
+		err = errors.New("消息序列化失败")
+		return
+	}
+
+	err = ch.Publish(
+		"",     // exchange
+		q.Name, // routing key
+		false,  // mandatory
+		false,  // immediate
+		amqp.Publishing{
+			ContentType:  "application/json",
+			Body:         body,
+			DeliveryMode: 2, //持久化
+			AppId:        os.Getenv("APP_NAME"),
+			Timestamp:    time.Now(),
+		},
+	)
+	if err != nil {
+		//记录mongo日志
+		if input.CurrentMode == "PRD" {
 			c.Insert(issue)
 		}
 		err = errors.New(queueName + "发送队列消息失败")
