6644d59b0bb06441fb4de5379409b567854dc5a5
louis <zhuaiman@hotmail.com>
2024-05-16 15:42:36 +0800

    数据输出行，直接转换对象结构

--- Convert.go
+++ Convert.go
@@ -3,6 +3,8 @@ package marisfrolg_utils
 import (
 	"math"
 	"math/big"
+	"reflect"
+	"time"
 )
 
 func ToFloat64(ori []byte) (re float64) {
@@ -45,9 +47,9 @@ func Substr(str string, start int, length int) string {
 	return string(rs[start:end])
 }
 
-//获取source的子串,如果start小于0或者end大于source长度则返回""
-//start:开始index，从0开始，包括0
-//end:结束index，以end结束，但不包括end
+// 获取source的子串,如果start小于0或者end大于source长度则返回""
+// start:开始index，从0开始，包括0
+// end:结束index，以end结束，但不包括end
 func SubString(source string, start int, end int) string {
 	var r = []rune(source)
 	length := len(r)
@@ -125,9 +127,90 @@ func Round(f float64, n int) float64 {
 	return math.Trunc((f+0.5/n10)*n10) / n10
 }
 
+func MapToStruct(data map[string]interface{}, result interface{}) error {
+	rv := indirect(reflect.ValueOf(result), false)
+	for key, value := range data {
+		fieldValue := rv.FieldByName(key)
+		if fieldValue.IsValid() {
+			valueValue := reflect.ValueOf(value)
+			if fieldValue.CanSet() && value != nil {
+				if fieldValue.Type().Name() == "Time" && valueValue.Type().Name() == "string" {
+					fieldValue.Set(reflect.ValueOf(parseOracleTime(value)))
+				} else {
+					fieldValue.Set(valueValue)
+				}
+			}
+		}
+	}
+	return nil
+}
 
+func indirect(v reflect.Value, decodingNull bool) reflect.Value {
+	// Issue #24153 indicates that it is generally not a guaranteed property
+	// that you may round-trip a reflect.Value by calling Value.Addr().Elem()
+	// and expect the value to still be settable for values derived from
+	// unexported embedded struct fields.
+	//
+	// The logic below effectively does this when it first addresses the value
+	// (to satisfy possible pointer methods) and continues to dereference
+	// subsequent pointers as necessary.
+	//
+	// After the first round-trip, we set v back to the original value to
+	// preserve the original RW flags contained in reflect.Value.
+	v0 := v
+	haveAddr := false
 
+	// If v is a named type and is addressable,
+	// start with its address, so that if the type has pointer methods,
+	// we find them.
+	if v.Kind() != reflect.Pointer && v.Type().Name() != "" && v.CanAddr() {
+		haveAddr = true
+		v = v.Addr()
+	}
+	for {
+		// Load value from interface, but only if the result will be
+		// usefully addressable.
+		if v.Kind() == reflect.Interface && !v.IsNil() {
+			e := v.Elem()
+			if e.Kind() == reflect.Pointer && !e.IsNil() && (!decodingNull || e.Elem().Kind() == reflect.Pointer) {
+				haveAddr = false
+				v = e
+				continue
+			}
+		}
 
+		if v.Kind() != reflect.Pointer {
+			break
+		}
 
+		if decodingNull && v.CanSet() {
+			break
+		}
 
+		// Prevent infinite loop if v is an interface pointing to its own address:
+		//     var v interface{}
+		//     v = &v
+		if v.Elem().Kind() == reflect.Interface && v.Elem().Elem() == v {
+			v = v.Elem()
+			break
+		}
+		if v.IsNil() {
+			v.Set(reflect.New(v.Type().Elem()))
+		}
 
+		if haveAddr {
+			v = v0 // restore original value after round-trip Value.Addr().Elem()
+			haveAddr = false
+		} else {
+			v = v.Elem()
+		}
+	}
+	return v
+}
+
+func parseOracleTime(input interface{}) (result time.Time) {
+	if _date, ok := input.(string); ok {
+		result, _ = time.ParseInLocation("2006-01-02T15:04:05", _date, CstZone)
+	}
+	return result
+}
